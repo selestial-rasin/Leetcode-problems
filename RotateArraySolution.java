83afe4203c896b785e8e0ae4c0b9e791ecec90db
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-22 12:00:39 +0530

    Create RotateArraySolution.java

---
+++ RotateArraySolution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class RotateArraySolution {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] a, int l, int r) {
+        while (l < r) {
+            int temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
