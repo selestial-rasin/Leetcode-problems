281797b939ed08188da7a8e36e0e7ab3999177a2
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-22 11:56:17 +0530

    Create BinarySearchSolve.java

---
+++ BinarySearchSolve.java
@@ -0,0 +1,22 @@
+
+public class BinarySearchSolve {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return -1;
+    }
+}
