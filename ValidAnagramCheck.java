f455a8216991f24a1f6c5c3a85ef796217bf7ecf
Selestial.Rasin <rasinsallai@gmail.com>
2025-11-22 12:01:56 +0530

    Create  ValidAnagramCheck.java

---
+++ ValidAnagramCheck.java
@@ -0,0 +1,22 @@
+public class ValidAnagramCheck {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) count[c - 'a']++;
+        for (char c : t.toCharArray()) count[c - 'a']--;
+
+        for (int n : count) {
+            if (n != 0) return false;
+        }
+        return true;
+    }
+}
